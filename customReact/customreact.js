4f712fcb8ef763d507348d3985ee5eb2eaed75b8
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2024-01-30 00:19:10 +0530

    custom react done

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,4 +1,29 @@
-const mainContainer = document.querySelector('#root')
+function CustomRender(ReactElement, container) {
+  // const domElement = document.createElement(ReactElement.type);
+  // domElement.innerHTML = ReactElement.children;
+  // domElement.setAttribute('href', ReactElement.props.href);
+  // domElement.setAttribute('target', ReactElement.props.target);
+  // container.appendChild(domElement);
 
+  const domElement = document.createElement(ReactElement.type);
+  domElement.innerHTML = ReactElement.children;
+  for (const prop in ReactElement.props) {
+    if (prop === 'children') {
+      continue;
+    }
+    domElement.setAttribute(prop, ReactElement.props[prop]);
+  }
+  container.appendChild(domElement);
+}
 
-rend
+const ReactElement = {
+  type: "a",
+  props: {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  children: "Click me here to go there",
+};
+
+const mainContainer = document.querySelector("#root");
+CustomRender(ReactElement, mainContainer);
