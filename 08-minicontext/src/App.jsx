7c9ac1c569af291f6b2c2408650445d0c2c3064e
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2024-02-07 19:08:37 +0530

    have problem

---
+++ 08-minicontext/src/App.jsx
@@ -0,0 +1,14 @@
+import Login from "./components/Login";
+import Profile from "./components/profile";
+
+export default function App() {
+  return (
+    <>
+      <h1>hey</h1>
+      <Login />
+      <Profile />
+    </>
+  )
+}
+
+
